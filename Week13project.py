6536e3b0f15460da25e34332073f44bef4407f9a
EC2 Default User <ec2-user@ip-172-31-39-212.ec2.internal>
2023-02-14 16:10:41 +0000

    initial commit

--- Week13project.py
+++ Week13project.py
@@ -4,11 +4,9 @@ cwdfiles = os.listdir(cwd)
 
 for file in cwdfiles:
     path = os.path.join(cwd, file)
-    file_attr = os.stat(path)
-
     mydict = { 
-        'file' : file, 
-        'size' : file_attr.st_size,
+        'name' : file, 
+        'size' : os.path.getsize(file),
         'path' : path
     }
     print(mydict)
