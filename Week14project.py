fa526f98b9df9581f5a8de4d3e3ec5fbc4d8ce2a
EC2 Default User <ec2-user@ip-172-31-39-212.ec2.internal>
2023-02-19 02:02:31 +0000

    commit

--- Week14project.py
+++ Week14project.py
@@ -1,7 +1,44 @@
-import boto3
+import boto3  #allows access to boto3
 
 dynamodb = boto3.resource(
     'dynamodb',
-    aws_access_key_id='*****',
-    aws_secret_access_key='*****',
+    aws_access_key_id='AKIAS6FLDRQ5YYONVTUB',
+    aws_secret_access_key='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',
     )
+
+
+# Create the DynamoDB table.
+table = dynamodb.create_table ( # Create the DynamoDB table.
+    TableName = 'DrinkChoice',
+       KeySchema = [
+        {
+            'AttributeName': 'Name',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'Drink',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'Name',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'Drink',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+
+table.wait_until_exists() # Waits until the table exists before finishing
+
+print(table) #returns confirmation of table creation
+
+
