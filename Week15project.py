6c131e3a1fb17885cbadc424078a0e30b5d86b21
EC2 Default User <ec2-user@ip-172-31-39-212.ec2.internal>
2023-02-27 05:41:01 +0000

    commit

--- Week15project.py
+++ Week15project.py
@@ -1,25 +1,41 @@
-import boto3
+import logging
 
-def create_queue():
-    sqs_client = boto3.client("sqs", region_name="us-east-1")
-    response = sqs_client.create_queue(
-        QueueName="Week15",
-        Attributes={
-            "DelaySeconds": "0",
-            "VisibilityTimeout": "60",  # 60 seconds
-        }
-    )
-    print(response)
+import time
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+sqs = boto3.resource('sqs')
+
+def create_sample_queue(name, queue_attributes=None):
+    if not queue_attributes:
+        queue_attributes = {}
+    try:
+        queue = sqs.create_queue(
+            QueueName=name,
+            Attributes=queue_attributes
+        )
+        logger.info("Created queue '%s' with URL=%s", name, queue.url)
+    except ClientError as error:
+        logger.exception("Couldn't create queue named '%s'.", name)
+        raise error
+    else:
+        return queue
     
+if __name__=="__main__":
+    # create standard queue
+    standard_queue_attributes = {
+            'MaximumMessageSize': str(1024),
+            'ReceiveMessageWaitTimeSeconds': str(20)
+        }    
+    standard_queue_name = "Week15pro"
+    standard_queue = create_sample_queue(standard_queue_name, standard_queue_attributes)
+    print(f"Created queue with URL: {standard_queue.url}.")
+
+   
     
-    
-    
-def get_queue_url():
-    sqs_client = boto3.client("sqs", region_name="us-east-1")
-    response = sqs_client.get_queue_url(
-        QueueName="Week15",
-    )
-    return response["QueueUrl"]
+
+ 
     
     
     
