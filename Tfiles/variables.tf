1b3665854c60cf790d5830f1fa4f7a065f9ea403
EC2 Default User <ec2-user@ip-172-31-57-213.ec2.internal>
2023-04-03 18:53:00 +0000

    ?

---
+++ Tfiles/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    type = string
+    default = "ami-00c39f71452c08778"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+
+
+
+variable "security_group" {
+    type = string
+    default = "t2.micro"
+}
+
+
