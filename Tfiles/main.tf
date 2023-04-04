5971014d9777948157e99aba728d4b6a08791ade
EC2 Default User <ec2-user@ip-172-31-57-213.ec2.internal>
2023-04-04 05:16:19 +0000

    week20

--- Tfiles/main.tf
+++ Tfiles/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "terrajenkinsec2" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  user_data = <<EOF
+  ami                      = var.ami
+  instance_type            = var.instance_type
+  vpc_security_group_ids   = [var.security_group_id]
+  user_data     = <<EOF
     #!/bin/bash
     yum update –y
     wget -O /etc/yum.repos.d/jenkins.repo \
@@ -13,42 +14,46 @@ resource "aws_instance" "terrajenkinsec2" {
     systemctl enable jenkins #Jenkins starts on boot
     systemctl start jenkins #starts Jenkins service now
     EOF
- 
+
+tags = {
+    Name = "Jenkins"
+  }
 }
+
 
 resource "aws_security_group" "JenkinsSG" {
   name        = "JenkinsSG"
-  description = "Allow https and ssh and Jenkins"
-  vpc_id      = aws_vpc.main.id
+  description = "Allow HTTPS/ssh and Jenkins"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
-  
-  ingress {
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["44.211.206.215/32"]
   }
-  
+
   ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_s3_bucket" "Jenkins_Artifact_Bucket" {
-  bucket = "Jenkins_Artifact_Bucket"
-  }
+resource "aws_s3_bucket" "jenkinsArtifactBucket" {
+  bucket = var.s3bucketname
+}
--- Tfiles/variables.tf
+++ Tfiles/variables.tf
@@ -1,19 +1,35 @@
 variable "ami" {
-    type = string
-    default = "ami-00c39f71452c08778"
+  type    = string
+  default = "ami-00c39f71452c08778"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
+}
+variable "instance_name" {
+  type    = string
+  default = "Jenkins"
 }
 
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-026f49c079f59f5df"
+}
 
 
 
 variable "security_group" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
+
+variable "cidr" {
+  description = "CIDR"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
 
 
